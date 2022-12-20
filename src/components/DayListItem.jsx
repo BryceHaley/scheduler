28fb42202831b4365bca9f4e23545f6fe1f02b3f
Bryce Haley <bhaley91@gmail.com>
2022-12-20 14:29:40 -0500

    interviewer and day list item

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./DayListItem.scss"
+import classNames from "classnames";
+
+const formatSpots = (spots) => {
+  return spots === 0 ? "no spots remaining" : 
+    spots === 1 ? "1 spot remaining" : spots + " spots remaining"; 
+};
+
+export default function DayListItem(props) {
+  
+  const listItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  const spotsRemainingText = formatSpots(props.spots);
+
+  return (
+    <li className = {listItemClass} 
+      onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsRemainingText}</h3>
+    </li>
+  );
+}
