d83a4c179886cfcba55451598282ce7be4b6ea51
Bryce Haley <bhaley91@gmail.com>
2023-01-03 15:08:41 -0800

    create appointment test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -35,9 +35,31 @@ describe("Appointments", () => {
 
   it("should edit an interview", () => {
     cy.visit("/");
+    cy.get('[alt="Edit"]')
+      .invoke('show')
+      .click();
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Bob Jones");
+      cy.get('[alt="Tori Malcolm"]')
+      .click();
+    cy.contains('Save')
+      .click();
+    
+    cy.contains('Bob Jones');
+    cy.contains('Tori Malcolm');    
+     
   })
-/*
+
   it("should cancel an interview", () => {
     cy.visit("/");
-  })*/
+    cy.get('[alt="Delete"]')
+      .invoke('show')
+      .click();
+    cy.contains("Confirm")
+      .click();
+
+
+    cy.contains("Archie Cohen").should('not.exist');
+  })
 })
